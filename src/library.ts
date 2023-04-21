7974c30fe4df68e40498719a2eeab1f83b2416b9
Allan Asp Christensen <allan@greenbow.dk>
2023-04-21 11:24:21 +0200

    Update library.ts

--- src/library.ts
+++ src/library.ts
@@ -1,6 +1,11 @@
 // This is the root file from where the package will be generated.
-export { default as HelloWorld } from './components/HelloWorld.vue';
-export { default as HelloWorlds } from './components/HelloWorlds.vue';
+export { default as HelloWorld } from "./components/HelloWorld.vue";
+export { default as HelloWorlds } from "./components/HelloWorlds.vue";
+export { default as AtomButton } from "./components/atoms/button/AtomButton.vue";
+export { default as AtomInput } from "./components/atoms/input/AtomInput.vue";
+export { default as AtomIcon } from "./components/atoms/icon/AtomIcon.vue";
+export { default as AtomTextArea } from "./components/atoms/textarea/AtomTextarea.vue";
+
 // import static stylesheets so that rollup will inlcude this in the package
 // note that scss variables need to be imported using the vite.config.ts
-import './styles/shared.css';
+import "./styles/shared.css";
